cd8538cf0b2e934ad75a72e848666bba452682e8
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-16 22:39:29 +0700

    Add validator to Blog model

--- app/models/Blog.js
+++ app/models/Blog.js
@@ -1,14 +1,41 @@
 'use strict';
-
 var mongoose	= require('../../config/db.js');
 var Schema		= mongoose.Schema;
+var validator = require('validator');
+
+var validateUrl = (string) => {
+  return validator.isURL(string, {
+    allow_underscores: true
+  });
+};
 
 var BlogSchema	= new Schema({
-  title : String,
+  title: {
+    type: String,
+    required: true
+  },
   content: String,
-  markdown: String,
-  image: String,
-  link: String,
+  markdown: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validateUrl,
+      message: '{VALUE} is not a valid URL!'
+    }
+  },
+  link: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validateUrl,
+      message: '{VALUE} is not a valid URL!'
+    }
+  },
   tags: Array,
   date: Date
 });
